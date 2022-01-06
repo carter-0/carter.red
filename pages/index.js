2a9753958f9863dc4809b94cfd1f128f2f5cd7ef
carter <carterannandale@gmail.com>
2022-01-06 08:18:42 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,54 +11,43 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="">carter.red</a>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className="card">
+            <Link href="/projects">
+              <a className="link"><h3>Projects &rarr;</h3></a>
+            </Link>
+            <Link href="/projects">
+              <p>Things i've made</p>
+            </Link>
+          </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://github.com/carter-0" className="card">
+            <h3>Github &rarr;</h3>
+            <p>github.com/carter-0</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://twitter.com/__carter"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Twitter &rarr;</h3>
+            <p>twitter.com/__carter</p>
+          </a>
+
+          <a
+            href="https://discord.com/users/498867264351633419"
+            className="card"
+          >
+            <h3>Discord &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              carter#6932
             </p>
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
