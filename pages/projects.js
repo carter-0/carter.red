a28abb3e49d684f322f4ffb738f1e8364c174306
carter <carterannandale@gmail.com>
2022-01-06 08:19:59 +0000

    add pages

---
+++ pages/projects.js
@@ -0,0 +1,20 @@
+export default function Projects() {
+    return (
+        <div>
+            <h1>i can't css so here you go</h1>
+            <hr />
+            <p>Topaz Bots - A SaaS Web application using mainly Docker, Flask and SQL to host twitter bots.</p>
+            <a href="https://topazbots.com">topazbots.com</a>
+            <hr />
+            <p>Bean Battles Hacks - A game cheat made in C#, sold at 2 price levels. Website made with Flask and Bootstrap, intergrating with Stripe, Paypal and Mailgun API's.</p>
+            <a href="https://beanbattleshacks.net">beanbattleshacks.net</a>
+            <hr />
+            <p>Crypto-To.me - a portfolio site for various crypto addresses. Made in 2 days with Flask and SQL.</p>
+            <a href="https://crypto-to.me">crypto-to.me</a>
+            <hr />
+            <p>CS Jobs - A data aggregator for Computer Science jobs, made with Flask and SQL - originally hosted at csjobs.net but now in an archived state on csjobs.carter.red.</p>
+            <a href="https://csjobs.carter.red">csjobs.carter.red</a>
+            <hr />
+        </div>
+    )
+}
